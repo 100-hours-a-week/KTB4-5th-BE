1f3ca3b5373477bc5d65eb36682d66e0afd36132
dlawnsdnjs <wns1628@gmail.com>
2026-09-19 23:07:00 +0900

    fix(test): 최신 User 생성자에 맞춰 테스트 준비 코드 수정

--- src/test/java/com/dameokja/backend/user/infrastructure/UserLockRepositoryTest.java
+++ src/test/java/com/dameokja/backend/user/infrastructure/UserLockRepositoryTest.java
@@ -1,6 +1,7 @@
 package com.dameokja.backend.user.infrastructure;
 
 import com.dameokja.backend.user.domain.User;
+import com.dameokja.backend.user.domain.UserCredentials;
 import com.dameokja.backend.support.MySqlJpaTest;
 import jakarta.persistence.LockModeType;
 import org.junit.jupiter.api.Test;
@@ -14,7 +15,8 @@ class UserLockRepositoryTest extends MySqlJpaTest {
 
     @Test
     void findsRequestedEntityWithWriteLock() {
-        User user = new User("회원이름", "default.png", "login1", "hash", null);
+        User user = new User("회원이름", "default.png",
+                new UserCredentials("login1", "hash", null));
         entityManager.persist(user);
         Long entityId = user.getId();
         flushAndClear();
