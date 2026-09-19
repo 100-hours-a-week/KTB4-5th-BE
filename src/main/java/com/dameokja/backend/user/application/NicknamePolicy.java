52203bba39932d022155b540f4be7f7b8bf334bb
dlawnsdnjs <wns1628@gmail.com>
2026-09-19 23:16:29 +0900

    refactor(user): 오류 코드 전달용 레코드 제거와 검증 호출 단순화

--- src/main/java/com/dameokja/backend/user/application/NicknamePolicy.java
+++ src/main/java/com/dameokja/backend/user/application/NicknamePolicy.java
@@ -1,18 +1,22 @@
 package com.dameokja.backend.user.application;
 
-import com.dameokja.backend.user.domain.UserExceptionCode;
+import static com.dameokja.backend.user.domain.UserExceptionCode.NICKNAME_REQUIRED;
+import static com.dameokja.backend.user.domain.UserExceptionCode.NICKNAME_LENGTH_INVALID;
+import static com.dameokja.backend.user.domain.UserExceptionCode.NICKNAME_FORMAT_INVALID;
+import static com.dameokja.backend.user.domain.UserExceptionCode.NICKNAME_PROHIBITED;
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
 public class NicknamePolicy {
-    private static final NicknameAndLoginIdValidator.ErrorCodes ERROR_CODES = new NicknameAndLoginIdValidator.ErrorCodes(
-            UserExceptionCode.NICKNAME_REQUIRED, UserExceptionCode.NICKNAME_LENGTH_INVALID,
-            UserExceptionCode.NICKNAME_FORMAT_INVALID, UserExceptionCode.NICKNAME_PROHIBITED);
     private final NicknameAndLoginIdValidator nicknameAndLoginIdValidator;
 
     public void validate(String nickname) {
-        nicknameAndLoginIdValidator.validate(nickname, ERROR_CODES);
+        nicknameAndLoginIdValidator.validateRequired(nickname, NICKNAME_REQUIRED);
+        nicknameAndLoginIdValidator.validateLength(nickname, NICKNAME_LENGTH_INVALID);
+        nicknameAndLoginIdValidator.validateCharacters(nickname, NICKNAME_FORMAT_INVALID);
+        nicknameAndLoginIdValidator.validateProhibitedWord(nickname, NICKNAME_PROHIBITED);
     }
 }
