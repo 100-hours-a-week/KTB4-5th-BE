9c905df7198912c43fbd31164085bd01c642692e
jeongminju <wjdalswn0405@naver.com>
2026-09-20 22:16:42 +0900

    refactor(global): 중복된 시간 설정 import 제거

--- src/main/java/com/dameokja/backend/global/config/JpaAuditingConfig.java
+++ src/main/java/com/dameokja/backend/global/config/JpaAuditingConfig.java
@@ -8,12 +8,10 @@ import java.time.ZoneOffset;
 import java.time.temporal.ChronoUnit;
 import java.util.Optional;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Import;
 import org.springframework.data.auditing.DateTimeProvider;
 
 @Configuration
 @EnableJpaAuditing(dateTimeProviderRef = "auditingDateTimeProvider")
-@Import(TimeConfig.class)
 public class JpaAuditingConfig {
     @Bean
     DateTimeProvider auditingDateTimeProvider(Clock clock) {
