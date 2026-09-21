ab24bea774f48f0a8a89d20cc79455482a0a470c
dlawnsdnjs <wns1628@gmail.com>
2026-09-21 17:04:15 +0900

    feat(health): Health Check endpoint 공개

--- src/main/java/com/dameokja/backend/global/security/SecurityConfig.java
+++ src/main/java/com/dameokja/backend/global/security/SecurityConfig.java
@@ -2,11 +2,25 @@ package com.dameokja.backend.global.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
     @Bean
     public static PasswordEncoder passwordEncoder() { return new BCryptPasswordEncoder(); }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(auth -> auth
+                .requestMatchers(
+                        "/actuator/health",
+                        "/actuator/health/**"
+                ).permitAll()
+                .anyRequest().authenticated()
+        );
+        return http.build();
+    }
 }
